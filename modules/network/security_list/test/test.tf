4e78f5852275720c6984b319a7b806bc7628239e
sogawa-yk <yuki.sogawa@oracle.com>
2024-04-18 04:43:11 +0000

    add security_list

--- modules/network/security_list/test/test.tf
+++ modules/network/security_list/test/test.tf
@@ -109,9 +109,7 @@ locals {
 }
 
 module "test_module" {
-  source = "./path/to/your/module/directory"
-
-  for_each        = local.test_data
+  source = "../"
   compartment_id  = var.compartment_id
   vcn_id          = "ocid1.vcn.oc1.ca-toronto-1.amaaaaaassl65iqa4bwogdr3jswhepteejebls3kyizyr4jkt4tzf25dafrq"
   security_lists  = local.test_data
